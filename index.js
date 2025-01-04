9ec1019a0ef47a2207d8287b1abddb25198fd3f6
TNT <139092950+TNTxTrick@users.noreply.github.com>
2025-01-04 18:34:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const app = express();
 const PORT = 3000;
 
 // Static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-// Endpoint chính để phục vụ trang HTML
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", async function (req, res) {
+	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // Đọc danh sách IP từ file
